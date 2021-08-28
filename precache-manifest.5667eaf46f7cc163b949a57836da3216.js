726794d732be8f089521ad2e036b2e67ce2e0c1f
Utkarsh <utkarsh.ffnf@gmail.com>
2021-08-29 03:45:33 +0530

    Updates

---
+++ precache-manifest.5667eaf46f7cc163b949a57836da3216.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303d5212e20369edb20489c22de1b8ba",
+    "url": "/unichat/index.html"
+  },
+  {
+    "revision": "23df4f830fbe30ed5259",
+    "url": "/unichat/static/css/main.37448acd.chunk.css"
+  },
+  {
+    "revision": "0b57b0aa7fabc268d9bf",
+    "url": "/unichat/static/js/2.db992231.chunk.js"
+  },
+  {
+    "revision": "cf4263b26236207b597a32c399243e67",
+    "url": "/unichat/static/js/2.db992231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23df4f830fbe30ed5259",
+    "url": "/unichat/static/js/main.275dd957.chunk.js"
+  },
+  {
+    "revision": "d81067fa5f1d98a40195",
+    "url": "/unichat/static/js/runtime-main.014caeaa.js"
+  }
+]);
